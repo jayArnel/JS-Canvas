060b104edd760e0a41dc149537b04336b7b80f2b
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-06-09 22:00:07 +0800

    update js

--- canvas.js
+++ canvas.js
@@ -3,24 +3,23 @@ $(document).ready(function (){
     // canvas variables
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
-    var width=canvas.width;
-    var height=canvas.height;
+    var width = c.width;
+    var height = c.height;
 
     var cellSize = 15
     var initialLength = 5;
     var snake = [];
 
-    var initDirectionX = 0;
-    var initDirectionY = -1;
+    var directionX = 0;
+    var directionY = -1;
 
     function init() {
-        var x = c.width / 2;
-        var y = c.height / 2;
+        var x = width / 2;
+        var y = height / 2;
         for (var i = 0; i < initialLength; i++) {
             addSnakeCell(x, y)
             y += cellSize;
         }
-
         requestAnimationFrame(move);
     }
     init();
@@ -31,8 +30,6 @@ $(document).ready(function (){
             y: y,
             width: cellSize,
             height: cellSize,
-            directionX: initDirectionX,
-            directionY: initDirectionY,
         };
         snake.push(cell);
     }
@@ -50,11 +47,29 @@ $(document).ready(function (){
         }
     }
 
-    function move(time){
-        for(var i=0;i<snake.length;i++){
-            snake[i].x += snake[i].directionX;
-            snake[i].y += snake[i].directionY;
+    $(window).on('keydown', changeDirection);
+
+    function changeDirection(e) {
+        var code = e.keyCode;
+        switch (code) {
+            case 37: directionX = -1; directionY = 0; break; //Left key
+            case 38: directionX = 0; directionY = -1; break; //Up key
+            case 39: directionX = 1; directionY = 0; break; //Right key
+            case 40: directionX = 0; directionY = 1; break; //Down key
         }
+    }
+
+    function move(time){
+        var headX = snake[0].x;
+        var headY = snake[0].y;
+
+        headX += directionX;
+        headY += directionY;
+        var tail = snake.pop(); //pops out the last cell
+        tail.x = headX;
+        tail.y = headY;
+        
+        snake.unshift(tail); //puts back the tail as the first cell
 
         draw();
 
